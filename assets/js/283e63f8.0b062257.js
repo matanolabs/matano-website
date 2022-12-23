db872156d0dacda97943fbe20af64bb118746526
Samrose-Ahmed <Samrose-Ahmed@users.noreply.github.com>
2022-12-23 09:08:30 +0000

    deploy: 0875ee072a9f34e2030715c23e57a2b8d0ee44b0

---
+++ assets/js/283e63f8.0b062257.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_matano_website=self.webpackChunk_matano_website||[]).push([[4157],{52844:e=>{e.exports=JSON.parse('{"label":"announcement","permalink":"/blog/tags/announcement","allTagsPath":"/blog/tags","count":1}')}}]);
---
+++ assets/js/a7023ddc.0551bed7.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_matano_website=self.webpackChunk_matano_website||[]).push([[1713],{53457:e=>{e.exports=JSON.parse('[{"label":"announcement","permalink":"/blog/tags/announcement","count":1}]')}}]);

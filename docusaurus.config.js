a3bac566ab27662a397b96c17b706e9f97b5908b
🐼 Samrose Ahmed 🐼 <samroseahmed@gmail.com>
2023-02-23 16:46:47 -0800

    touch
    Signed-off-by: 🐼 Samrose Ahmed 🐼 <samroseahmed@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,7 +3,7 @@
 
 const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
-
+// --
 function ldJson(obj) {
   return {
     tagName: "script",
